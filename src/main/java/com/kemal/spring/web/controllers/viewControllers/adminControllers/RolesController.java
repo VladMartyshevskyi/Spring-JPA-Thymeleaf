967914c80073491df13a151978ace6bf504af7d6
Vlad <vladmartishevskii@gmail.com>
2018-07-16 17:27:24 +0300

    Refactored

--- src/main/java/com/kemal/spring/web/controllers/viewControllers/adminControllers/RolesController.java
+++ src/main/java/com/kemal/spring/web/controllers/viewControllers/adminControllers/RolesController.java
@@ -3,98 +3,66 @@ package com.kemal.spring.web.controllers.viewControllers.adminControllers;
 import com.kemal.spring.domain.Role;
 import com.kemal.spring.service.RoleService;
 import com.kemal.spring.web.dto.UserDto;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/adminPage")
 public class RolesController {
-	
+
 	@Autowired
-    private RoleService roleService;
+	private RoleService roleService;
 
-    @GetMapping("/roles")
-    public ModelAndView showRoles() {
-        ModelAndView modelAndView = new ModelAndView("adminPage/role/roles");
-        modelAndView.addObject("roles", roleService.findAll());
-        return modelAndView;
-    }
+	@GetMapping("/roles")
+	public ModelAndView showRoles() {
+		ModelAndView modelAndView = new ModelAndView("adminPage/role/roles");
+		modelAndView.addObject("roles", roleService.findAll());
+		return modelAndView;
+	}
 
-    @GetMapping("/roles/{id}")
-    public ModelAndView getEditRoleForm(@PathVariable Long id) {
-        Role role = roleService.findById(id);
-        ModelAndView modelAndView = new ModelAndView("adminPage/role/editRole");
-        modelAndView.addObject("role", role);
-        return modelAndView;
-    }
+	@GetMapping("/roles/{id}")
+	public ModelAndView getEditRoleForm(@PathVariable Long id) {
+		Role role = roleService.findById(id);
+		ModelAndView modelAndView = new ModelAndView("adminPage/role/editRole");
+		modelAndView.addObject("role", role);
+		return modelAndView;
+	}
 
-    @PostMapping("/roles/{id}")
-    public String updateRole(Model model, @PathVariable Long id,
-                             @ModelAttribute("oldRole") @Valid final Role role,
-                             BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+	@PostMapping("/roles/{id}")
+	public String updateRole(Model model, @PathVariable Long id, @ModelAttribute("oldRole") @Valid final Role role,
+			RedirectAttributes redirectAttributes) {
+		Role existingRole = roleService.findByName(role.getName());
+		if (existingRole != null && !existingRole.getName().equals(role.getName())) {
+			throw new RuntimeException("Role name is already taken");
+		} else {
+			roleService.save(role);
+			redirectAttributes.addFlashAttribute("roleHasBeenUpdated", true);
+			return "redirect:/adminPage/roles";
+		}
 
-        String formWithErrors = "adminPage/role/editRole";
-        Role persistedRole = roleService.findById(id);
-        List<Role> allRoles = roleService.findAll();
+	}
 
-     //   boolean roleNameAlreadyExists = roleService.checkIfRoleNameIsTaken(allRoles, role, persistedRole);
-        boolean roleNameAlreadyExists = false;
-        boolean hasErrors = false;
+	@GetMapping("/roles/newRole")
+	public String getAddNewRoleForm(Model model) {
+		model.addAttribute("newUser", new UserDto());
+		return "adminPage/role/newRole";
+	}
 
-        if (roleNameAlreadyExists) {
-            bindingResult.rejectValue("name", "roleNameAlreadyExists", "Oops!  There is already a role registered with the name provided.");
-            hasErrors = true;
-        }
-
-        if (bindingResult.hasErrors()) hasErrors = true;
-
-        if (hasErrors) {
-            model.addAttribute("role", role);
-            model.addAttribute("org.springframework.validation.BindingResult.role", bindingResult);
-            return formWithErrors;
-        } else {
-            roleService.save(role);
-            redirectAttributes.addFlashAttribute("roleHasBeenUpdated", true);
-            return "redirect:/adminPage/roles";
-        }
-    }
-
-    @GetMapping("/roles/newRole")
-    public String getAddNewRoleForm(Model model) {
-        model.addAttribute("newUser", new UserDto());
-        return "adminPage/role/newRole";
-    }
-
-    @PostMapping("/roles/newRole")
-    public String saveNewRole(Model model, @ModelAttribute("newRole") @Valid final Role newRole,
-                              BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-
-        Role roleNameAlreadyExists = roleService.findByName(newRole.getName());
-        boolean hasErrors = false;
-        String formWithErrors = "adminPage/role/newRole";
-
-        if (roleNameAlreadyExists != null) {
-            bindingResult.rejectValue("name", "usernameAlreadyExists", "Oops!  There is already a role registered with the name provided.");
-            hasErrors = true;
-        }
-
-        if (bindingResult.hasErrors()) hasErrors = true;
-
-        if (hasErrors) return formWithErrors;
-
-        else {
-            roleService.save(newRole);
-            redirectAttributes.addFlashAttribute("roleHasBeenSaved", true);
-            return "redirect:/adminPage/roles";
-        }
-    }
+	@PostMapping("/roles/newRole")
+	public String saveNewRole(Model model, @ModelAttribute("newRole") @Valid final Role role,
+			RedirectAttributes redirectAttributes) {
+		Role existingRole = roleService.findByName(role.getName());
+		if (existingRole != null && !existingRole.getName().equals(role.getName())) {
+			throw new RuntimeException("Role name is already taken");
+		} else {
+			roleService.save(role);
+			redirectAttributes.addFlashAttribute("roleHasBeenSaved", true);
+			return "redirect:/adminPage/roles";
+		}
+	}
 }
